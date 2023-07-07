5280b873b3929f4842dc8545ea3b8261c63609c9
John Phonxaya <phonxayajohn@gmail.com>
2023-07-07 10:41:24 -0500

    sequence lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0,1])
+    else:
+        fibonacci_list = [0, 1]
+        while len(fibonacci_list) < length:
+            next_number = fibonacci_list[-1] + fibonacci_list[-2]
+
+            fibonacci_list.append(next_number)
+       
+        print(fibonacci_list)
+    
